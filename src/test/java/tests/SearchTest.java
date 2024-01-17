1f49511924b44fce2dec5f9cd10c8f61ea723845
Nataliia B <114429981+NataliiaBor1@users.noreply.github.com>
2024-01-17 12:35:23 -0800

    Sprint 3. TC1361-02 - Displaying All Matches for the Searched Keyword (#67)
    Co-authored-by: Liudmila Plucci <efloorplan14@gmail.com>
    Co-authored-by: ArinaJur <66705803+ArinaJur@users.noreply.github.com>

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,12 +1,18 @@
 package tests;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
-public class SearchTest extends BaseTest{
+public class SearchTest extends BaseTest {
+
+    private final String EXISTING_KEYWORD = "stand";
 
     @Test
     public void testSearchByNotExistingKeyWord() {
@@ -19,5 +25,22 @@ public class SearchTest extends BaseTest{
         assertThat(getPage()
                 .locator("div:has(input[placeholder='Search']) + div>div"))
                 .hasText("Nothing found. Try to use other key words");
+    }
+
+    @Test
+    public void testSearchByExistingKeyWord() {
+        List<Locator> listOfMatches = new ArrayList<>();
+
+        listOfMatches.add(getPage().locator("button:not(:has(> *))"));
+
+        getPage().getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Study guide")).click();
+
+        getPage().getByPlaceholder("Search").fill(EXISTING_KEYWORD);
+
+        for (int i = 3; i <= listOfMatches.size(); i++) {
+            assertThat(getPage()
+                    .locator("listOfMatches.get(i)"))
+                    .hasText(EXISTING_KEYWORD);
+        }
     }
 }
