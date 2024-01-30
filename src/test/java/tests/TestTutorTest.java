73cb0bf58a0a9af8005b27d407dd44f0e6f148ce
YuliaMatusevich <115679291+YuliaMatusevich@users.noreply.github.com>
2024-01-30 15:08:48 -0600

    Sprint 5. Restore test TC-1364 (#129)
    Co-authored-by: ArinaJur <66705803+ArinaJur@users.noreply.github.com>

--- src/test/java/tests/TestTutorTest.java
+++ src/test/java/tests/TestTutorTest.java
@@ -1,9 +1,7 @@
 package tests;
 
 import com.microsoft.playwright.Locator;
-import io.qameta.allure.Description;
-import io.qameta.allure.Story;
-import io.qameta.allure.TmsLink;
+import io.qameta.allure.*;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
@@ -223,5 +221,28 @@ public class TestTutorTest extends BaseTest {
                 "If FAIL: Points after running test (" + mainSectionPointsAfter
                         + ") are not greater then points before running test (" + mainSectionPointsBefore + ").\n"
         );
+    }
+    @Test(
+            testName = "LMS-1364 Запуск тестов. https://app.qase.io/case/LMS-1364",
+            description = "TC1364-01 - Running Test in Study Guide with Gold Subscription"
+    )
+    @Description("Objective: To confirm that users with a Gold subscription can successfully run a test in the Study Guide section.")
+    @Story("Study Guide")
+    @TmsLink("ufe2bohbd0sy")
+    @Severity(SeverityLevel.NORMAL)
+    public void testTestIsRunWhenOpenFromStudyGuide() {
+        TestTutorPage testTutorPage =
+                new HomePage(getPage()).init()
+                        .clickStudyGuideMenu()
+                        .scrollToPageBottom()
+                        .clickYesButton();
+
+        Assert.assertEquals(
+                TestData.LIST_OF_TUTOR_TEST_FOOTER_BUTTONS, testTutorPage.listOfButtonNamesInFooter()
+        );
+        assertThat(testTutorPage.getTestQuestion()).containsText(TestData.QUESTION_MARK);
+        Assert.assertTrue(
+                testTutorPage.getAnswersCount() > 0,
+                "If FAIL: The multiple-choice test should contain at least one answer.\n");
     }
 }
